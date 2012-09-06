a9510d1ea541d6e7465b702de71588dbc737f9ac
toran billups <toranb@gmail.com>
2012-09-05 22:19:51 -0500

    added the person repository rest example

--- ember/static/script/app/person.js
+++ ember/static/script/app/person.js
@@ -18,6 +18,7 @@ PersonApp.PersonView = Ember.View.extend({
 });
 
 PersonApp.Person = Ember.Object.extend({
+  id: 0,
   username: null
 });
 
@@ -40,16 +41,61 @@ PersonApp.Router = Ember.Router.create({
       addPerson: function(router, username) {
         var person = PersonApp.Person.create({ username: username });
         router.get('personController').addPerson(person);
+        PersonApp.PersonRepository.add(person);
+      },
+      updatePerson: function(router, event) {
+        PersonApp.PersonRepository.update(event.context);
       },
       removePerson: function(router, event) {
         var person = event.context;
         router.get('personController').removePerson(person);
+        PersonApp.PersonRepository.remove(person);
       },
       connectOutlets: function(router) {
-        router.get('applicationController').connectOutlet('person');
+        router.get('applicationController').connectOutlet('person', PersonApp.PersonRepository.find());
       }
     })
   })
+});
+
+PersonApp.PersonRepository = Ember.Object.create({
+  people: [],
+  url: 'http://localhost:8000/people',
+  find: function(){
+    var that = this;
+    $.getJSON(this.url, {}, function(response){
+      response.forEach(function(user){
+        var person = PersonApp.Person.create({ id: user['id'], username: user['username'] });
+        that.people.addObject(person);
+      }, this)
+    });
+    return this.people;
+  },
+  add: function(person) {
+    $.ajax({
+      url: this.url,
+      type: 'POST',
+      data: 'username=%@'.fmt(person.username),
+      success: function(response) { person.id = response.id; },
+      error: function(response) { console.log('add failed-tell the user or retry'); }
+    })
+  },
+  update: function(person) {
+    $.ajax({
+      url: '%@/%@'.fmt(this.url, person.id),
+      type: 'PUT',
+      data: 'username=%@'.fmt(person.username),
+      error: function(response) { console.log('update failed-tell the user or retry'); }
+    })
+  },
+  remove: function(person) {
+    $.ajax({
+      url: '%@/%@'.fmt(this.url, person.id),
+      type: 'DELETE',
+      data: {},
+      error: function(response) { console.log('delete failed-tell the user or retry'); }
+    })
+  }
 });
 
 $(function () {
